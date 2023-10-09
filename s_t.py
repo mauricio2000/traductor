e99aa967f842b1f501dc38ea030ce3ab4981e626
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-10-08 20:44:39 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -272,23 +272,13 @@ if opc=="Texto":
                 tld = "co.za"            
             
             display_output_text = st.checkbox("Mostrar el texto")
-            def text_to_speech2(text, tld):
-    
-                    tts = gTTS(text,"es", tld, slow=False)
-                    try:
-                        my_file_name = text[0:20]
-                    except:
-                        my_file_name = "audio"
-                    tts.save(f"temp/{my_file_name}.mp3")
-                    return my_file_name, text
             if st.button("convertir"):
-                        result, output_text = text_to_speech2(text, tld)
-                        audio_file = open(f"temp/{result}.mp3", "rb")
-                        audio_bytes = audio_file.read()
-                        st.markdown(f"## Tú audio:")
-                        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-                    
-                        if display_output_text:
-                            st.markdown(f"## Texto de salida:")
-                            st.write(f" {output_text}")
+                result, output_text = text_to_speech(input_language, output_language, text, tld)
+                audio_file = open(f"temp/{result}.mp3", "rb")
+                audio_bytes = audio_file.read()
+                st.markdown(f"## Tú audio:")
+                st.audio(audio_bytes, format="audio/mp3", start_time=0)
             
+                if display_output_text:
+                    st.markdown(f"## Texto de salida:")
+                    st.write(f" {output_text}")
