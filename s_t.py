4565683bf59ea321909a81f50853ab031c072070
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-10-08 20:24:32 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -291,11 +291,9 @@ if opc=="Texto":
             display_output_text = st.checkbox("Mostrar el texto")
             
             if st.button("convertir"):
-                result, output_text = text_to_speech(input_language, output_language, text, tld)
-                audio_file = open(f"temp/{result}.mp3", "rb")
-                audio_bytes = audio_file.read()
-                st.markdown(f"## Tú audio:")
-                st.audio(audio_bytes, format="audio/mp3", start_time=0)
+                
+                st.markdown(f"## Texto de salida:")
+                st.write(f" {output_text}")
             
                 if display_output_text:
                     st.markdown(f"## Texto de salida:")
