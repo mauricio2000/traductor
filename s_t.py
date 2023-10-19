8c8da409c42e3fc985d4ac3ddbe0618b547cb352
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-10-19 07:38:41 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -303,9 +303,21 @@ if opc=="Texto":
 if opc=="Imagen":
 
         st.subheader("Has elegido IMAGEN")
-        
+        st.write("Toma foto a lo que deseas traducir")
         img_file_buffer = st.camera_input("Toma una Foto")
+        if img_file_buffer is not None:
+            # To read image file buffer with OpenCV:
+            bytes_data = img_file_buffer.getvalue()
+            cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+        if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+        else:
+         cv2_img= cv2_img
+    
         
+        img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        text=pytesseract.image_to_string(img_rgb)
         
         in_lang = st.selectbox(
                 "Selecciona el lenguaje de Entrada",
@@ -317,14 +329,7 @@ if opc=="Imagen":
                 ("Inglés", "Español", "Frances","Bengali", "Coreano", "Mandarín", "Japonés","Portugues"),
             )
         
-        st.write("Toma foto a lo que deseas traducir")
-        imagen = st.camera_input("Capturar foto",label_visibility='visible')
-        cv2.imwrite("image.png", imagen)
-        
-        if imagen is not None:                
-                img_cv = cv2.imread('image,png')
-                img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-                text = str(pytesseract.image_to_string(img_rgb))
+       
         
         if True:            
             if in_lang == "Inglés":
