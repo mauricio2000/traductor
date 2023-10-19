55fd832e5dc467569195bbe650cc080b108ff187
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-10-19 07:43:05 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -308,13 +308,9 @@ if opc=="Imagen":
         if img_file_buffer is not None:
             # To read image file buffer with OpenCV:
             bytes_data = img_file_buffer.getvalue()
-            cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-        
-    
-        
-        img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-        text=pytesseract.image_to_string(img_rgb)
+            cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)        
+            img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+            text=pytesseract.image_to_string(img_rgb)
         
         in_lang = st.selectbox(
                 "Selecciona el lenguaje de Entrada",
